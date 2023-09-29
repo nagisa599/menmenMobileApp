a293ceeaec51b4039fe34e5c0fc52918832bdd5e
nagisa599 <nagi46460510@icloud.com>
2023-09-29 13:27:19 +0900

    画面遷移

---
+++ src/screens/CouponScreen.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function CouponScreen() {
+  return (
+    <View>
+      <Text>これは、クーポン画面です</Text>
+    </View>
+  );
+}
---
+++ src/screens/RankingScreen.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function RankingScreen() {
+  return (
+    <View>
+      <Text>これは、ランキングz画面です</Text>
+    </View>
+  );
+}
