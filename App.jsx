b2467635bc5b500259715aef7635fb7d6ae31b43
Kohei Komuro <82309237+k-sml@users.noreply.github.com>
2023-10-01 21:58:41 +0900

    Revert "Tab component"

--- App.jsx
+++ App.jsx
@@ -1,39 +1,20 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import CouponScreen from './src/screens/CouponScreen';
-import FriendScreen from './src/screens/FriendScreen';
-import HomeScreen from './src/screens/HomeScreen';
-import MypageScreen from './src/screens/MypageScree';
-import RankingScreen from './src/screens/RankingScreen';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const Tab = createBottomTabNavigator();
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Signup"
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#C0C0C0',
-          },
-          headerTitle: 'MenMen',
-          headerTintColor: '#000000',
-          headerBackTitle: 'back',
-          tabBarInactiveTintColor: 'black',
-          tabBarActiveTintColor: 'tomato',
-          headerTitleStyle: {
-            fontStyle: 'italic',
-            fontSize: 40,
-          },
-        }}
-      >
-        <Tab.Screen name="メニュー" component={HomeScreen} />
-        <Tab.Screen name="クーポン" component={CouponScreen} />
-        <Tab.Screen name="ランキング" component={RankingScreen} />
-        <Tab.Screen name="フレンド" component={FriendScreen} />
-        <Tab.Screen name="マイページ" component={MypageScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
