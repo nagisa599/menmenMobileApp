bf1918869022680f2eb46ad89e7b52e01b632aa0
Kohei Komuro <82309237+k-sml@users.noreply.github.com>
2023-10-26 18:22:42 +0900

    title移動

--- src/screens/TitleScreen.jsx
+++ src/screens/TitleScreen.jsx
@@ -1,16 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import {
-  View, StyleSheet, Text, Alert,
-} from 'react-native';
+import { View, StyleSheet, Alert } from 'react-native';
 import {
   getFirestore, getDoc, doc,
 } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-import Tab from '../components/Tab';
 import CircleTitle from '../components/CircleTitle';
 
-export default function TitleScreen(props) {
-  const { navigation } = props;
+export default function TitleScreen() {
   const [title, setTitle] = useState(0);
   const setTitleImage = async () => {
     try {
@@ -30,46 +26,17 @@ export default function TitleScreen(props) {
   }, []);
   return (
     <View style={styles.container}>
-      <View style={styles.tabContainer}>
-        <Tab
-          label="ランキング"
-          onPress={() => {
-            navigation.navigate('RankingScreen');
-          }}
-        />
-        <Tab label="称号" onPress={() => {}} active />
-      </View>
-      <View>
-        <View style={styles.firstTextContainer}>
-          <Text style={styles.firstText}>あなたの称号は・・・</Text>
-        </View>
-        <CircleTitle title={title} />
-        <View style={styles.lastTextContainer}>
-          <Text style={styles.lastText}>です！</Text>
-        </View>
-      </View>
+      <CircleTitle title={title} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  tabContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    paddingVertical: 30,
-  },
-  firstTextContainer: {
-    marginTop: 40,
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    paddingLeft: 30,
-  },
-  firstText: {
-    fontSize: 40,
+  container: {
+    marginBottom: 50,
   },
   circle: {
     alignItems: 'center',
-    paddingVertical: 40,
   },
   lastTextContainer: {
     flexDirection: 'row',
