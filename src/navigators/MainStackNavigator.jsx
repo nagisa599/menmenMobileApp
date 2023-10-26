db3f16a0331a637ceaa56544821e446b98b7cc6f
Kohei Komuro <82309237+k-sml@users.noreply.github.com>
2023-10-26 18:04:27 +0900

    header left

--- src/navigators/MainStackNavigator.jsx
+++ src/navigators/MainStackNavigator.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { bool, func } from 'prop-types';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AnimatedSplashScreen from '../screens/AnimatedSplashScreen';
 import MypageScreen from '../screens/MypageScreen';
 import ComingCheckScreen from '../screens/ComingCheckScreen';
 import Generator from '../components/Generator';
 import MainTabs from './TabScreen';
+import { convertFirestoreTimestampToDate, formatDateToYYYYMMDD } from '../utils/Data';
 
 const Stack = createNativeStackNavigator();
 
@@ -25,8 +28,78 @@ function MyheaderLeft({ navigation }) {
   );
 }
 
+function MyheaderRight({ navigation, visited, setVisited }) {
+  const iconColor = visited ? '#ccc' : '#000';
+  return (
+    <TouchableOpacity
+      onPress={() => navigation.navigate('ComingCheck', { setVisited })}
+      disabled={visited}
+    >
+      <Ionicons
+        name="qr-code-outline"
+        size={24}
+        color={iconColor}
+      />
+    </TouchableOpacity>
+  );
+}
+
+MyheaderRight.propTypes = {
+  visited: bool.isRequired,
+  setVisited: func.isRequired,
+};
+
 export default function MainStackNavigator({ isSplashVisible, setSplashVisible }) {
   const navigation = useNavigation();
+  const [visited, setVisited] = useState(true);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const db = getFirestore();
+
+    // マイページ開く際にFirebaseの認証状態が完全に復元されてないとエラーが起きるためonAuthStateChangedで監視する
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const currentUserUid = user.uid;
+        const userPath = `users/${currentUserUid}/`;
+        const userInfoDocRef = doc(db, userPath);
+        try {
+          const userDoc = await getDoc(userInfoDocRef);
+          if (userDoc.exists()) {
+            const userData = userDoc.data();
+            let lastVisitDate = null;
+            if (userData.times && userData.times.length > 0) {
+              lastVisitDate = userData.times[userData.times.length - 1];
+              lastVisitDate = convertFirestoreTimestampToDate(lastVisitDate);
+              lastVisitDate = formatDateToYYYYMMDD(lastVisitDate);
+            }
+
+            const today = formatDateToYYYYMMDD(new Date());
+
+            if (lastVisitDate === today) {
+              setVisited(true);
+            } else {
+              setVisited(false);
+            }
+          } else {
+            console.log('ユーザー情報がない');
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      } else {
+        console.log('ユーザーはログインしていません');
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  useEffect(() => {
+  }, [visited]);
+
   if (isSplashVisible) {
     return <AnimatedSplashScreen setSplashVisible={setSplashVisible} />;
   }
@@ -39,6 +112,14 @@ export default function MainStackNavigator({ isSplashVisible, setSplashVisible }
         options={{
           // eslint-disable-next-line react/no-unstable-nested-components
           headerLeft: () => <MyheaderLeft navigation={navigation} />,
+          // eslint-disable-next-line react/no-unstable-nested-components
+          headerRight: () => (
+            <MyheaderRight
+              navigation={navigation}
+              setVisited={setVisited}
+              visited={visited}
+            />
+          ),
           headerTitleStyle: {
             fontSize: 24,
             fontFamily: 'Arial',
